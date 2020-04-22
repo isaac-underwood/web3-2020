bfad2eee4d8c058711f1bf030df9a696b9e29ebb
Isaac Underwood <undeis1@student.op.ac.nz>
2020-04-22 21:41:39 +1200

    update ajax functions for api routes

--- Checkpoint2_Basic_Style/static/js/main.js
+++ Checkpoint2_Basic_Style/static/js/main.js
@@ -1,4 +1,4 @@
-$.get('countries', function() {
+$.get('/countries', function() {
     console.log("Countries loaded");
 }).fail(function() {
     console.log("Countries not loaded");
@@ -6,10 +6,36 @@ $.get('countries', function() {
     console.log("this is printed no matter what");
 });
 
-$.get('countries/<country_id>', function() {
+$.get('/countries/<country_name>', function() {
     console.log("Country found");
 }).fail(function() {
     console.log("Country not found");
 }).always(function() {
     console.log("this is printed no matter what");
 });
+
+$.post('/countries/new', function() {
+    console.log("Country added");
+}).fail(function() {
+    console.log("Error country not added")
+});
+
+$.ajax('/countries/<country_name>', {
+        type: 'PUT',
+    })
+    .done(function() {
+        console.log("Country updated");
+    })
+    .fail(function() {
+        console.log("Error updating country");
+    });
+
+$.ajax('/countries/<country_name>', {
+        type: 'DELETE',
+    })
+    .done(function() {
+        console.log("Country deleted");
+    })
+    .fail(function() {
+        console.log("Error deleting country");
+    });
