d7196cc8d88504f07181bf0f9d7197f160a14b31
Isaac Underwood <undeis1@student.op.ac.nz>
2020-05-26 20:43:48 +1200

    fix loadData method not creating country objects correctly

--- Checkpoint2_Basic_Style/app.py
+++ Checkpoint2_Basic_Style/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template
 from mongoengine import *
+from pymongo import MongoClient
 import csv
 import os
 
@@ -8,9 +9,14 @@ app.config.from_object('config')
 connect()
 
 countries = {}
+
 class Country(Document):
     name = StringField()
     data = DictField()
+
+class Data(EmbeddedDocument):
+    name = StringField()
+    content = DictField()
 
 @app.route('/')
 @app.route('/index')
@@ -27,6 +33,35 @@ def inspiration():
 
 @app.route('/loadData')
 def loadData():
+    for file in os.listdir(app.config['FILES_FOLDER']):
+        filename = os.fsdecode(file)
+        path = os.path.join(app.config['FILES_FOLDER'],filename)
+        f = open(path)
+        r = csv.DictReader(f)
+        d = list(r)
+        for data in d:
+            country_ = Country() # a blank placeholder country
+            dict = {} # a blank placeholder data dict
+            for key in data: # iterate through the header keys
+                if key == "country":
+                    # check if this country already exists in the db
+                    if not Country.objects(name=data[key]).count():
+                        country_.name = data[key] # if the country does not exist, we can use the new blank country we created above, and set the name
+                    else: 
+                        country_ = Country.objects(name=data[key]).first()
+                        dict = country_.data
+                    # if the country already exists, replace the blank country with the existing country from the db, and replace the blank dict with the current country's 
+                    # data      
+                else:
+                    f = filename.replace(".csv","") # we want to trim off the ".csv" as we can't save anything with a "." as a mongodb field name
+                    if f in dict: # check if this filename is already a field in the dict
+                        dict[f][key] = data[key] # if it is, just add a new subfield which is key : data[key] (value)
+                    else:
+                        dict[f] = {key:data[key]} # if it is not, create a new object and assign it to the dict
+                # add the data dict to the country
+                country_.data = dict
+            # save the country
+            country_.save()
     return 'Success', 200
 
 @app.route('/countries', methods=['GET'])
@@ -35,47 +70,14 @@ def getCountries(country_name=None):
     # country_ = Country(name='New Zealand')
     # country_.save()
     # for file in os.listdir(app.config['FILES_FOLDER']):
-    #     filename = os.fsdecode(file)
+    #     filename = o s.fsdecode(file)
     #     path = os.path.join(app.config['FILES_FOLDER'],filename)
     #     f = open(path)
     #     r = csv.reader(f)
     #     d = list(r)
     #     for data in d:
     #         print(data)
-    for file in os.listdir(app.config['FILES_FOLDER']):
-        filename = os.fsdecode(file)
-        path = os.path.join(app.config['FILES_FOLDER'],filename)
-        f = open(path)
-        r = csv.DictReader(f) 
-        d = list(r)
-        for data in d:
-            country = Country() # a blank placeholder country
-            dict = {} # a blank placeholder data dict
-            print(data)
-            for key in data: # iterate through the header keys
-                if key == "country":
-                    # check if this country already exists in the db
-                    if not checkCountryExists(country):
-                        country.name = data[key] # if the country does not exist, we can use the new blank country we created above, and set the name
-                    else: 
-                        # country = Country.objects.
-                        country = Country.query.filter_by(name=data[key]).first()
-                        dict = country.data
-                    # if the country already exists, replace the blank country with the existing country from the db, and replace the blank dict with the current country's 
-                    # data                
-                else:
-                    f = filename.replace(".csv","") # we want to trim off the ".csv" as we can't save anything with a "." as a mongodb field name
-                    if f in dict: # check if this filename is already a field in the dict
-                        dict[f][key] = data[key] # if it is, just add a new subfield which is key : data[key] (value)
-                    else:
-                        dict[f] = {key:data[key]} # if it is not, create a new object and assign it to the dict
-
-                # add the data dict to the country
-                country.data = dict
-            # save the country
-            country.save()
-    return 'test', 200
-
+    return Country.objects.to_json(), 200
 
 @app.route('/countries/<country_name>', methods=['PUT', 'DELETE'])
 def updateCountry(country_name):
